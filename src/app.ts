3ad08b7cb57f722396332cd690d939531472c652
Acarulo <augusto.e.caro@gmail.com>
2023-08-02 23:20:35 -0300

    MongoDB setup & interface init

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,15 @@ import express from "express";
 import cors from "cors";
 
 import {router} from "./routes";
+import db from "./config/mongo"
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(router);
+
+db().then(() => console.log("DB is ready"));
 
 app.listen(port, () => {
     console.log(`Listening to port no. ${port}`);
